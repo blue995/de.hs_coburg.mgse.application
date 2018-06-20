a1b801ce62cfef027597d56947d00293047cae2b
John <jonathan.braat@live.com>
2018-06-20 08:47:35 +0200

    mock object curriculum

--- frontend/src/app/shared/models/curriculum.ts
+++ frontend/src/app/shared/models/curriculum.ts
@@ -1,9 +1,29 @@
-import {CurriculumEntry} from './curriculum-entry';
+import {Professor} from './professor';
+import {CourseType, ExamType} from './module';
 
 export class Curriculum {
   id: number;
   version: number;
   year: number;
   completeName: String;
+  curriculumStudySections: CurriculumStudySection[];
+}
+
+export class CurriculumStudySection {
+  completeName: String;
   curriculumEntries: CurriculumEntry[];
+}
+
+export class CurriculumEntry {
+  semester: number;
+  rota: String;
+  moduleCompleteName: String;
+  moduleAbbreviation: String;
+  ects: number;
+  semesterHours: number;
+  moduleCourseTypes: CourseType[];
+  moduleExamTypes: ExamType[];
+  aidList: String[];
+  responsiblePerson: Professor[];       // can be more than one in the case of "Seminar" and similar courses
+  testers: Professor[];
 }
